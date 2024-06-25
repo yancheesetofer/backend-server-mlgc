67f6f77612a1565ac66ae5eb05afe5281deed4f1
Yan Christofer Silalahi <yanchristof@gmail.com>
2024-06-25 17:10:13 +0700

    machine learning model on gcp backend

---
+++ src/services/storeData.js
@@ -0,0 +1,15 @@
+import { Firestore } from '@google-cloud/firestore';
+
+const storeData = async (id, data) => {
+  const db = new Firestore({
+    keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS,
+    projectId: 'submissionmlgc-yanchristofer',
+    databaseId: 'predictions',
+  });
+
+  const predictCollection = db.collection('predictions');
+
+  return predictCollection.doc(id).set(data);
+};
+
+export default storeData;
